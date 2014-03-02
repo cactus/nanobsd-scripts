ad534d78b267e67a5c4052da4353c1d795e12bed
elij <elij@wundrd.net>
2014-03-01 18:31:44 -0800

    update

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 H=$(dirname "$0")
 cd "${H}/./../"
+mkdir -p output
 sudo sh /usr/src/tools/tools/nanobsd/nanobsd.sh -n -c nanos/binary_build.nano
--- scripts/fetch-dists.sh
+++ scripts/fetch-dists.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
-DISTS="base.txz kernel.txz docs.txz games.txz"
+DISTS="base.txz kernel.txz docs.txz games.txz src.txz"
 H=$(dirname "$0")
-cd "${H}/./../dists"
+DDIR="${H}/./../dists"
+mkdir -p $DDIR
+cd $DDIR
 for tarball in ${DISTS}; do
     curl --continue - -LO "http://ftp.freebsd.org/pub/FreeBSD/releases/amd64/amd64/10.0-RELEASE/${tarball}"
 done
